6c72a74ac286b86d64d6374f115f714bae59c194
saulocastillo3 <saulocastillo3@gmail.com>
2024-08-02 22:34:43 -0400

    part2

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,47 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+     return cart.reduce((total, item) => 
+     total + item.quantity * 
+     parseFloat(item.cost.slice(1)), 0).toFixed(2);
  
   };
 
   const handleContinueShopping = (e) => {
    
+    e.preventDefault();
+    onContinueShopping();
+
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+      if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item));
+    }
    
   };
 
   const handleRemove = (item) => {
+        dispatch(removeItem(item));
+
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     return (item.quantity * parseFloat(item.cost.slice(1))).toFixed(2);
   };
+
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   return (
     <div className="cart-container">
